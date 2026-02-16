0f498ada49dbb91d072395cdb6788ec1d8c857af
LeonKDev <Leonkruijer@outlook.com>
2026-02-16 09:51:14 +0100

    touch input

--- Mars_Crash/Assets/Scripts/Touch/TouchManager.cs
+++ Mars_Crash/Assets/Scripts/Touch/TouchManager.cs
@@ -2,15 +2,25 @@ using UnityEngine;
 
 public class TouchManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Camera _mainCamera;
+    [SerializeField] private GameObject _player;
+    [SerializeField] private Vector3 _offset = new(0, 0.5f, 0);
+
+    private void Update()
     {
-        
+        MovePlayerToTouch();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void MovePlayerToTouch()
     {
-        
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            RaycastHit hit;
+            Ray rayOrigin = _mainCamera.ScreenPointToRay(Input.GetTouch(0).position);
+
+            if (Physics.Raycast(rayOrigin, out hit))
+                _player.transform.position = hit.point + _offset;
+        }
     }
 }
+
