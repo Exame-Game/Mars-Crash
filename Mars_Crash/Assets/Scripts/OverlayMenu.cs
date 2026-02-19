edc566e068b40c493189f7d20427ffdab55f9dbb
LeonKDev <Leonkruijer@outlook.com>
2026-02-19 15:16:19 +0100

    volume control

--- Mars_Crash/Assets/Scripts/OverlayMenu.cs
+++ Mars_Crash/Assets/Scripts/OverlayMenu.cs
@@ -1,7 +1,13 @@
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class OverlayMenu : MonoBehaviour
 {
+    public AudioMixer audioMixer;
+    public Slider musicSlider;
+    public Slider sfxSlider;
+
     [SerializeField] private GameObject _overlayMenu;
     [SerializeField] private GameObject _overlayMenuButton;
 
@@ -16,10 +22,36 @@ public class OverlayMenu : MonoBehaviour
     {
         _overlayMenu.SetActive(false);
         _overlayMenuButton.SetActive(true);
+        SaveVolume();
     }
     public void MainMenu()
     {
         SceneSwitch.Instance.ScenenChanger("MainMenu");
         MusicManager.Instance.PlayMusic("MainMenu", 0.5f);
+        SaveVolume();
+    }
+
+    public void UpdateMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("MusicVolume", volume);
+    }
+
+    public void UpdateSFXVolume(float volume)
+    {
+        audioMixer.SetFloat("SFXVolume", volume);
+    }
+
+    public void SaveVolume()
+    {
+        audioMixer.GetFloat("MusicVolume", out float musicVolume);
+        PlayerPrefs.SetFloat("MusicVolume", musicVolume);
+
+        audioMixer.GetFloat("SFXVolume", out float sfxVolume);
+        PlayerPrefs.SetFloat("SFXVolume", sfxVolume);
+    }
+    public void LoadVolume()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+        sfxSlider.value = PlayerPrefs.GetFloat("SFXVolume");
     }
 }
--- Mars_Crash/Assets/Scripts/UI/MainMenu.cs
+++ Mars_Crash/Assets/Scripts/UI/MainMenu.cs
@@ -1,9 +1,16 @@
+using System;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioMixer audioMixer;
+    public Slider musicSlider;
+    public Slider sfxSlider;
+
+    //todo less references 
     [SerializeField] private GameObject _musicSlider;
     [SerializeField] private GameObject _sfxSlider;
     [SerializeField] private GameObject _startButton;
@@ -14,6 +21,7 @@ public class MainMenu : MonoBehaviour
     
     void Start()
     {
+        LoadVolume();
         MusicManager.Instance.PlayMusic("MainMenu");
     }
 
@@ -23,7 +31,7 @@ public class MainMenu : MonoBehaviour
         MusicManager.Instance.PlayMusic("Level 1", 0.5f);
     }
 
-    public void Settings()
+    public void OpenSettings()
     {
         _startButton.SetActive(false);
         _ufo.SetActive(false);
@@ -38,9 +46,34 @@ public class MainMenu : MonoBehaviour
         _musicSlider.SetActive(false);
         _sfxSlider.SetActive(false);
         _exitSettingsButton.SetActive(false);
-        
+
         _startButton.SetActive(true);
         _ufo.SetActive(true);
         _settingsButton.SetActive(true);
+        SaveVolume();
+    }
+
+    public void UpdateMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("MusicVolume", volume);
+    }
+
+    public void UpdateSFXVolume(float volume)
+    {
+        audioMixer.SetFloat("SFXVolume", volume);
+    }
+
+    public void SaveVolume()
+    {
+        audioMixer.GetFloat("MusicVolume", out float musicVolume);
+        PlayerPrefs.SetFloat("MusicVolume", musicVolume);
+
+        audioMixer.GetFloat("SFXVolume", out float sfxVolume);
+        PlayerPrefs.SetFloat("SFXVolume", sfxVolume);
+    }
+    public void LoadVolume()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+        sfxSlider.value = PlayerPrefs.GetFloat("SFXVolume");
     }
 }
