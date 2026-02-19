14703da9f606757a6594530b6edaeb5d5831b4da
LeonKDev <Leonkruijer@outlook.com>
2026-02-19 14:30:50 +0100

    settings in main menu

--- Mars_Crash/Assets/Scripts/UI/MainMenu.cs
+++ Mars_Crash/Assets/Scripts/UI/MainMenu.cs
@@ -1,9 +1,17 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Audio;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private GameObject _musicSlider;
+    [SerializeField] private GameObject _sfxSlider;
+    [SerializeField] private GameObject _startButton;
+
+    [SerializeField] private GameObject _ufo;
+    [SerializeField] private GameObject _settingsButton;
+    [SerializeField] private GameObject _exitSettingsButton;
+    
     void Start()
     {
         MusicManager.Instance.PlayMusic("MainMenu");
@@ -13,5 +21,26 @@ public class MainMenu : MonoBehaviour
     {
         SceneSwitch.Instance.ScenenChanger("Alienlevel");
         MusicManager.Instance.PlayMusic("Level 1", 0.5f);
+    }
+
+    public void Settings()
+    {
+        _startButton.SetActive(false);
+        _ufo.SetActive(false);
+        _settingsButton.SetActive(false);
+
+        _exitSettingsButton.SetActive(true);
+        _musicSlider.SetActive(true);
+        _sfxSlider.SetActive(true);
+    }
+    public void ExitSettings()
+    {
+        _musicSlider.SetActive(false);
+        _sfxSlider.SetActive(false);
+        _exitSettingsButton.SetActive(false);
+        
+        _startButton.SetActive(true);
+        _ufo.SetActive(true);
+        _settingsButton.SetActive(true);
     }
 }
