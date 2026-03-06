91f1aad3d9f34cd14b2d525f541a67aee3fc8567
LeonKDev <Leonkruijer@outlook.com>
2026-03-06 11:53:32 +0100

    volume slider and UFO

--- Mars_Crash/Assets/Scripts/Audio/VolumeSettings.cs
+++ Mars_Crash/Assets/Scripts/Audio/VolumeSettings.cs
@@ -8,27 +8,43 @@ public class VolumeSettings : MonoBehaviour
     public Slider musicSlider;
     public Slider sfxSlider;
 
+
+    void Awake()
+    {
+        SaveVolume();
+    }
     public void UpdateMusicVolume(float volume)
     {
-        audioMixer.SetFloat("MusicVolume", volume);
+        audioMixer.SetFloat("MusicVolume", DecibelToVolume(volume));
     }
 
     public void UpdateSFXVolume(float volume)
     {
-        audioMixer.SetFloat("SFXVolume", volume);
+        audioMixer.SetFloat("SFXVolume", DecibelToVolume(volume));
     }
 
     public void SaveVolume()
     {
         audioMixer.GetFloat("MusicVolume", out float musicVolume);
-        PlayerPrefs.SetFloat("MusicVolume", musicVolume);
+        PlayerPrefs.SetFloat("MusicVolume", VolumeToDecibel(musicVolume));
 
         audioMixer.GetFloat("SFXVolume", out float sfxVolume);
-        PlayerPrefs.SetFloat("SFXVolume", sfxVolume);
+        PlayerPrefs.SetFloat("SFXVolume", VolumeToDecibel(sfxVolume));
     }
     public void LoadVolume()
     {
         musicSlider.value = PlayerPrefs.GetFloat("MusicVolume");
         sfxSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+    }
+    public float DecibelToVolume(float volume)
+    {
+        float decibelVolume = Mathf.Log10(Mathf.Max(volume, 0.0001f)) * 20f;
+        return decibelVolume;
+    }
+
+    public float VolumeToDecibel(float volume)
+    {
+        float decibelVolume = Mathf.Pow(10f, volume / 20f);
+        return decibelVolume;
     }
 }
