3ec25a3b3953bb833a8e808489332fd367da3e23
Merijn Florie <merijn@florie.nl>
2026-03-09 00:01:55 +0100

    feat: added NodeButton to NodeSystem

--- Mars_Crash/Assets/Scripts/path/ferron sollution/Node.cs
+++ Mars_Crash/Assets/Scripts/path/ferron sollution/Node.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,46 +6,4 @@ public class Node : MonoBehaviour
     public List<Node> ConnectedNodes;
     public List<Node> ConectionBlackList;
     public bool Occupied;
-}
-
-public class NodeButton : Node
-{
-    public event Action OnPressed;
-    public event Action OnReleased;
-    public bool IsPressed;
-}
-
-public class NodeButtonManager : MonoBehaviour
-{
-    [SerializeField] private List<NodeButton> buttons;
-    private GameObject buttonPrefab;
-
-    private void Awake()
-    {
-        Initialize();
-    }
-
-    private void Initialize()
-    {
-        foreach (NodeButton button in buttons)
-        {
-            button.OnPressed += () => OnButtonPressed(button);
-            button.OnReleased += () => OnButtonReleased(button);
-        }
-    }
-
-    private void OnButtonPressed(NodeButton button)
-    {
-        button.IsPressed = true;
-    }
-
-    private void OnButtonReleased(NodeButton button)
-    {
-        button.IsPressed = false;
-    }
-    
-    private void SpawnButton(GameObject buttonPrefab)
-    {
-        Instantiate(buttonPrefab, transform.position, Quaternion.identity, transform);
-    }
 }
---
+++ Mars_Crash/Assets/Scripts/path/ferron sollution/NodeButton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class NodeButton : Node
+{
+    public event Action OnPressed;
+    public event Action OnReleased;
+    public bool IsPressed;
+
+    public void Press()
+    {
+        IsPressed = true;
+        OnPressed?.Invoke();
+    }
+
+    public void Release()
+    {
+        IsPressed = false;
+        OnReleased?.Invoke();
+    }
+}
