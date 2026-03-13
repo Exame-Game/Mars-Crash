04056e3a3091d1dd2dd3e590179c11666eb57a19
Manderijn <99815029+GaMerijn2@users.noreply.github.com>
2026-03-13 09:38:24 +0100

    Revert "Stable Build - Sprint 2 (#10)" (#11)
    This reverts commit 597a7f5828244cf50f9f19d7fd72ac4244dd6279.

--- Mars_Crash/Assets/Scripts/InteractableSystem/Base/InteractableStates.cs
+++
@@ -1,6 +0,0 @@
-public enum InteractableStates
-{
-    Idle,
-    Selected,
-    Moving,
-}
--- Mars_Crash/Assets/Scripts/path/ferron sollution/Node.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Node : MonoBehaviour
-{
-    public List<Node> ConnectedNodes;
-    public List<Node> ConectionBlackList;
-    public bool Occupied;
-}
